391d3553cc92a1b1d978457b252405dec557e4dd
ipcrm <mcadorette@gmail.com>
2018-01-26 13:25:53 -0500

    spark, basic framework inplace

--- src/main/java/com/puppet/sample/App.java
+++ src/main/java/com/puppet/sample/App.java
@@ -1,14 +1,33 @@
 package com.puppet.sample;
-import static spark.Spark.*;
 
-/**
- * Hello world!
- *
- */
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
+
 public class App
 {
 
   public static void main(String[] args) {
-      get("/", (req, res) -> "Hello World");
+    get("/", (request, response) -> {
+      response.redirect("/en");
+      return null;
+    });
+
+    get("/:lang", App::helloWorld, new ThymeleafTemplateEngine());
+
   }
+
+  public static ModelAndView helloWorld(Request req, Response res) {
+    Map<String, Object> params = new HashMap<>();
+    params.put("lang",req.params(":lang"));
+    return new ModelAndView(params, "index");
+  }
+
 }
