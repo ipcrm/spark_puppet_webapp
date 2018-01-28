a30881e30ba3eab0b8a195a787263669e3218e40
ipcrm <mcadorette@gmail.com>
2018-01-28 00:15:47 -0500

    adding route tests

--- src/test/java/com/puppet/sample/AppTest.java
+++ src/test/java/com/puppet/sample/AppTest.java
@@ -1,38 +1,16 @@
 package com.puppet.sample;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+import org.junit.Rule;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
-/**
- * Unit test for simple App.
- */
+import spark.servlet.SparkApplication;
+
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testMsg() {
+        assertEquals("Hello World!", new App().enMsg());
     }
 }
---
+++ src/test/java/com/puppet/sample/TestRoutes.java
@@ -0,0 +1,65 @@
+package com.puppet.sample;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.AfterClass;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import spark.Spark;
+import spark.utils.IOUtils;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestRoutes 
+{
+    @BeforeClass
+    public static void beforeClass() {
+      App.main(null);
+    }
+   
+    @AfterClass
+    public static void afterClass() {
+      Spark.stop();
+    }
+
+    @Test
+    public void testEnMsg() {
+        TestResponse res = request("GET", "/en");
+        assertEquals(200, res.status);
+        assertTrue(res.body.contains("Hello World!"));
+    }
+
+    private TestResponse request(String method, String path) {
+		try {
+			URL url = new URL("http://localhost:4567" + path);
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod(method);
+			connection.setDoOutput(true);
+			connection.connect();
+			String body = IOUtils.toString(connection.getInputStream());
+			return new TestResponse(connection.getResponseCode(), body);
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail("Sending request failed: " + e.getMessage());
+			return null;
+		}
+	}
+
+	private static class TestResponse {
+
+		public final String body;
+		public final int status;
+
+		public TestResponse(int status, String body) {
+			this.status = status;
+			this.body = body;
+		}
+
+    }
+}
